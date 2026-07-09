79fbbda6d3c04a0af55e761786ebea2958877c23
vama-shivantra <235020113+vama-shivantra@users.noreply.github.com>
2026-07-09 10:36:30 +0530

    Configure GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "export",
   trailingSlash: false,
+  basePath: "/shivantra.github.io",
+  assetPrefix: "/shivantra.github.io/",
 };
 
 export default nextConfig;
